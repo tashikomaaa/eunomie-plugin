1c1b4da91b1848fe50114a5b517388f47b9d0054
aldwin <aldwin@mytroopers.com>
2023-11-15 19:43:49 +0100

    add login

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,22 +1,50 @@
 import React, { Component } from 'react'
 
-export default class Login extends Component {
+
+export default class Login extends Component<{}, {email: string, password: string}> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+    async login() {
+        await fetch('http://172.232.54.227:3000/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+        fetch('',)
+    }
     render() {
         return (
             <div className="Login">
-        
-            <div className="input-container">
-              <input type="text" placeholder="Username"/>
-              <i className="zmdi zmdi-account zmdi-hc-lg"></i>
+
+                <div className="input-container">
+                    <input type="text" value={this.state.email}  onChange={(e) => this.setState({email: e.target.value})} placeholder="email" />
+                    <i className="zmdi zmdi-account zmdi-hc-lg"></i>
+                </div>
+
+                <div className="input-container">
+                    <input type="password" value={this.state.password} onChange={(e) => this.setState({password: e.target.value})}placeholder="mot de pasee" />
+                    <i className="zmdi zmdi-lock zmdi-hc-lg"></i>
+                </div>
+
+                <button type="submit" onClick={async () => await this.login()}>Connection</button>
             </div>
-            
-            <div className="input-container">
-              <input type="password" placeholder="Password"/>
-              <i className="zmdi zmdi-lock zmdi-hc-lg"></i>
-            </div>
-            
-            <button type="submit">Log In</button>
-          </div>
         )
     }
 }
