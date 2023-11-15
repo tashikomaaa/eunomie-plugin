b3b9e4a2743bd2bbb28e5e056ed242a843394bf0
aldwin <aldwin@mytroopers.com>
2023-11-15 19:31:20 +0100

    add login

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -3,26 +3,20 @@ import React, { Component } from 'react'
 export default class Login extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="card">
-                    <div className="card-header">
-                        <h2>Login</h2>
-                    </div>
-                    <div className="card-body">
-                        <form>
-                            <div className="form-group">
-                                <label htmlFor="username">Username</label>
-                                <input type="text" id="username" name="username" required />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">Password</label>
-                                <input type="password" id="password" name="password" required />
-                            </div>
-                            <button type="submit">Sign In</button>
-                        </form>
-                    </div>
-                </div>
+            <div className="Login">
+        
+            <div className="input-container">
+              <input type="text" placeholder="Username"/>
+              <i className="zmdi zmdi-account zmdi-hc-lg"></i>
             </div>
+            
+            <div className="input-container">
+              <input type="password" placeholder="Password"/>
+              <i className="zmdi zmdi-lock zmdi-hc-lg"></i>
+            </div>
+            
+            <button type="submit">Log In</button>
+          </div>
         )
     }
 }
